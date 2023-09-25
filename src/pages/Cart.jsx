d8e3f70eb1de62980a7dded3c90fae796191d12c
Siddhantsingh1230 <Siddhantsingh9023134084@gmail.com>
2023-09-25 19:25:51 +0530

    changed back to home in cart btn

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -46,6 +46,28 @@ const Cart = () => {
           </h1>
         </div>
         <div className="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
+          {/* Sub total */}
+          <div className="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
+            <div className="mb-2 flex justify-between">
+              <p className="text-gray-700">Subtotal</p>
+              <p className="text-gray-700">$129.99</p>
+            </div>
+            <div className="flex justify-between">
+              <p className="text-gray-700">Shipping</p>
+              <p className="text-gray-700">$4.99</p>
+            </div>
+            <hr className="my-4" />
+            <div className="flex justify-between">
+              <p className="text-lg font-bold">Total</p>
+              <div className="">
+                <p className="mb-1 text-lg font-bold">$134.98 USD</p>
+                <p className="text-sm text-gray-700">including VAT</p>
+              </div>
+            </div>
+            <button className="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600">
+              Check out
+            </button>
+          </div>
           <div className="rounded-lg md:w-2/3">
             {products.map((item) => (
               <div
@@ -112,28 +134,7 @@ const Cart = () => {
               </div>
             ))}
           </div>
-          {/* Sub total */}
-          <div className="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
-            <div className="mb-2 flex justify-between">
-              <p className="text-gray-700">Subtotal</p>
-              <p className="text-gray-700">$129.99</p>
-            </div>
-            <div className="flex justify-between">
-              <p className="text-gray-700">Shipping</p>
-              <p className="text-gray-700">$4.99</p>
-            </div>
-            <hr className="my-4" />
-            <div className="flex justify-between">
-              <p className="text-lg font-bold">Total</p>
-              <div className="">
-                <p className="mb-1 text-lg font-bold">$134.98 USD</p>
-                <p className="text-sm text-gray-700">including VAT</p>
-              </div>
-            </div>
-            <button className="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600">
-              Check out
-            </button>
-          </div>
+          
         </div>
         <Link
           to="/"
