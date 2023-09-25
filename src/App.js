8726f60bc81e6cebe075b8663aed575731119db8
Siddhantsingh1230 <Siddhantsingh9023134084@gmail.com>
2023-09-25 16:45:37 +0530

    added login,signup skeletons and changed api names

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Home from './pages/Home';
+import {BrowserRouter as Router , Routes , Route} from "react-router-dom";
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 const App = () => {
   return (
     <>
-      <Home/>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/login" element={<Login/>}/>
+          <Route exact path="/signup" element={<Signup/>}/>
+        </Routes>
+      </Router>
     </>
   )
 }
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <>
+      Signup
+    </>
+  )
+}
+
+export default Signup
