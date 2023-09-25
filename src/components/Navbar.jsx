07962cf91a8c0c3a01a7fa5bbd7234654964c655
Siddhantsingh1230 <Siddhantsingh9023134084@gmail.com>
2023-09-25 15:09:18 +0530

    changed nav styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -138,7 +138,7 @@ const Navbar = ({ children }) => {
                       classProp={"w-60  mr-1"}
                       inputProp={"border-[1.5px]"}
                     />
-                    <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                    <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-sky-800 focus:ring-offset-2 focus:ring-offset-gray-800">
                       <span className="absolute -inset-0.5" />
                       <span className="sr-only">Open main menu</span>
 
