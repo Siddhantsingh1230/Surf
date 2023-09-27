62f9d4ded66130aaa14b6ce27cc7a6761e309c54
Siddhantsingh1230 <Siddhantsingh9023134084@gmail.com>
2023-09-27 23:27:32 +0530

    added padding in quick home btn in mobile scrns

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -8,7 +8,7 @@ const Category = () => {
     <>
       <Link to="/">
         <RippleBtn classes={"fixed left-5 top-3 bg-white w-auto px-2 max-sm:px-4 z-20"}>
-          <img src={HomeGif} className="max-sm:w-5 max-sm:h-5 w-6 h-6" alt="" />
+          <img src={HomeGif} className="max-sm:w-4 max-sm:h-4 w-6 h-6" alt="" />
         </RippleBtn>
       </Link>
       <section>
