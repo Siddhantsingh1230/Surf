6ae8a53bf7f8f51b8bc17fca79bdf30b3ed471e7
Siddhantsingh1230 <Siddhantsingh9023134084@gmail.com>
2023-09-27 23:24:55 +0530

    added padding in quick home btn in mobile scrns

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -7,39 +7,39 @@ const Category = () => {
   return (
     <>
       <Link to="/">
-        <RippleBtn classes={"fixed left-5 top-3 bg-white w-auto px-2 max-sm:px-2"}>
+        <RippleBtn classes={"fixed left-5 top-3 bg-white w-auto px-2 max-sm:px-4 z-20"}>
           <img src={HomeGif} className="max-sm:w-5 max-sm:h-5 w-6 h-6" alt="" />
         </RippleBtn>
       </Link>
       <section>
-        <div class="max-w-screen-xl px-4 py-8 mx-auto sm:px-6 sm:py-12 lg:px-8">
-          <header class="text-center">
-            <h2 class="text-xl font-bold text-gray-900 sm:text-3xl">
+        <div className="max-w-screen-xl px-4 py-8 mx-auto sm:px-6 sm:py-12 lg:px-8">
+          <header className="text-center">
+            <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">
               New Category
             </h2>
 
-            <p class="max-w-md mx-auto mt-4 text-gray-500">
+            <p className="max-w-md mx-auto mt-4 text-gray-500">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque
               praesentium cumque iure dicta incidunt est ipsam, officia dolor
               fugit natus?
             </p>
           </header>
 
-          <ul class="grid grid-cols-1 gap-4 mt-8 lg:grid-cols-3">
+          <ul className="grid grid-cols-1 gap-4 mt-8 lg:grid-cols-3">
             <li>
-              <a href="#" class="relative block group">
+              <a href="#" className="relative block group">
                 <img
                   src="https://images.unsplash.com/photo-1618898909019-010e4e234c55?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
                   alt=""
-                  class="object-cover w-full transition duration-500 aspect-square group-hover:opacity-90"
+                  className="object-cover w-full transition duration-500 aspect-square group-hover:opacity-90"
                 />
 
-                <div class="absolute inset-0 flex flex-col items-start justify-end p-6">
-                  <h3 class="text-xl font-medium text-white">
+                <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
+                  <h3 className="text-xl font-medium text-white">
                     Casual Trainers
                   </h3>
 
-                  <span class="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white">
+                  <span className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white">
                     Shop Now
                   </span>
                 </div>
@@ -47,37 +47,37 @@ const Category = () => {
             </li>
 
             <li>
-              <a href="#" class="relative block group">
+              <a href="#" className="relative block group">
                 <img
                   src="https://images.unsplash.com/photo-1624623278313-a930126a11c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
                   alt=""
-                  class="object-cover w-full transition duration-500 aspect-square group-hover:opacity-90"
+                  className="object-cover w-full transition duration-500 aspect-square group-hover:opacity-90"
                 />
 
-                <div class="absolute inset-0 flex flex-col items-start justify-end p-6">
-                  <h3 class="text-xl font-medium text-white">Winter Jumpers</h3>
+                <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
+                  <h3 className="text-xl font-medium text-white">Winter Jumpers</h3>
 
-                  <span class="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white">
+                  <span className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white">
                     Shop Now
                   </span>
                 </div>
               </a>
             </li>
 
-            <li class="lg:col-span-2 lg:col-start-2 lg:row-span-2 lg:row-start-1">
-              <a href="#" class="relative block group">
+            <li className="lg:col-span-2 lg:col-start-2 lg:row-span-2 lg:row-start-1">
+              <a href="#" className="relative block group">
                 <img
                   src="https://images.unsplash.com/photo-1593795899768-947c4929449d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80"
                   alt=""
-                  class="object-cover w-full transition duration-500 aspect-square group-hover:opacity-90"
+                  className="object-cover w-full transition duration-500 aspect-square group-hover:opacity-90"
                 />
 
-                <div class="absolute inset-0 flex flex-col items-start justify-end p-6">
-                  <h3 class="text-xl font-medium text-white">
+                <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
+                  <h3 className="text-xl font-medium text-white">
                     Skinny Jeans Blue
                   </h3>
 
-                  <span class="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white">
+                  <span className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white">
                     Shop Now
                   </span>
                 </div>
@@ -87,16 +87,16 @@ const Category = () => {
         </div>
       </section>
       <section>
-        <div class="max-w-screen-xl px-4 py-8 mx-auto sm:py-12 sm:px-6 lg:px-8">
-          <div class="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:items-stretch">
-            <div class="grid p-6 bg-gray-100 rounded place-content-center sm:p-8">
-              <div class="max-w-md mx-auto text-center lg:text-left">
+        <div className="max-w-screen-xl px-4 py-8 mx-auto sm:py-12 sm:px-6 lg:px-8">
+          <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:items-stretch">
+            <div className="grid p-6 bg-gray-100 rounded place-content-center sm:p-8">
+              <div className="max-w-md mx-auto text-center lg:text-left">
                 <header>
-                  <h2 class="text-xl font-bold text-gray-900 sm:text-3xl">
+                  <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">
                     Watches
                   </h2>
 
-                  <p class="mt-4 text-gray-500">
+                  <p className="mt-4 text-gray-500">
                     Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                     Quas rerum quam amet provident nulla error!
                   </p>
@@ -104,47 +104,47 @@ const Category = () => {
 
                 <a
                   href="#"
-                  class="inline-block px-12 py-3 mt-8 text-sm font-medium text-white transition bg-gray-900 border border-gray-900 rounded hover:shadow focus:outline-none focus:ring"
+                  className="inline-block px-12 py-3 mt-8 text-sm font-medium text-white transition bg-gray-900 border border-gray-900 rounded hover:shadow focus:outline-none focus:ring"
                 >
                   Shop All
                 </a>
               </div>
             </div>
 
-            <div class="lg:col-span-2 lg:py-8">
-              <ul class="grid grid-cols-2 gap-4">
+            <div className="lg:col-span-2 lg:py-8">
+              <ul className="grid grid-cols-2 gap-4">
                 <li>
-                  <a href="#" class="block group">
+                  <a href="#" className="block group">
                     <img
                       src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1598&q=80"
                       alt=""
-                      class="object-cover w-full rounded aspect-square"
+                      className="object-cover w-full rounded aspect-square"
                     />
 
-                    <div class="mt-3">
-                      <h3 class="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4">
+                    <div className="mt-3">
+                      <h3 className="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4">
                         Simple Watch
                       </h3>
 
-                      <p class="mt-1 text-sm text-gray-700">$150</p>
+                      <p className="mt-1 text-sm text-gray-700">$150</p>
                     </div>
                   </a>
                 </li>
 
                 <li>
-                  <a href="#" class="block group">
+                  <a href="#" className="block group">
                     <img
                       src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1598&q=80"
                       alt=""
-                      class="object-cover w-full rounded aspect-square"
+                      className="object-cover w-full rounded aspect-square"
                     />
 
-                    <div class="mt-3">
-                      <h3 class="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4">
+                    <div className="mt-3">
+                      <h3 className="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4">
                         Simple Watch
                       </h3>
 
-                      <p class="mt-1 text-sm text-gray-700">$150</p>
+                      <p className="mt-1 text-sm text-gray-700">$150</p>
                     </div>
                   </a>
                 </li>
