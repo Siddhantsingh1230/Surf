44e4d26c14ecfcfd51b8f3d9ca1b238312bfa33d
Siddhantsingh1230 <Siddhantsingh9023134084@gmail.com>
2023-09-27 23:17:48 +0530

    added margin in quick home btn in mobile scrns

--- src/pages/Deals.jsx
+++ src/pages/Deals.jsx
@@ -7,7 +7,7 @@ const Deals = () => {
   return (
     <>
       <Link to="/">
-        <RippleBtn classes={"fixed left-5 top-3 bg-white w-auto px-2 max-sm:px-2"}>
+        <RippleBtn classes={"fixed left-5 top-3 bg-white w-auto px-2 max-sm:px-3"}>
           <img src={HomeGif} className="max-sm:w-5 max-sm:h-5 w-6 h-6" alt="" />
         </RippleBtn>
       </Link>
