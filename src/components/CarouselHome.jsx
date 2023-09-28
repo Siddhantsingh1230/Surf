db32076fb96609771565b8e0dbe083321a639b50
Siddhantsingh1230 <Siddhantsingh9023134084@gmail.com>
2023-09-28 08:54:03 +0530

    modified

--- src/components/CarouselHome.jsx
+++ src/components/CarouselHome.jsx
@@ -4,14 +4,15 @@ import Banner1 from "../assets/images/banner1.jpg";
 import Banner2 from "../assets/images/banner2.jpeg";
 import Banner3 from "../assets/images/banner3.jpg";
 import Banner4 from "../assets/images/banner4.jpg";
+import Autoplay from 'embla-carousel-autoplay'
 
 const EmblaCarousel = () => {
-  const [emblaRef] = useEmblaCarousel({ loop: true });
-  const slides = [Banner1, Banner2, Banner3, Banner4];
+  const [emblaRef] = useEmblaCarousel({ loop: true ,dragFree: true }, [Autoplay({ delay: 7500 })]);
+  const slides = [Banner3, Banner4 , Banner1, Banner2 ];
   const imageByIndex = (index) => slides[index % slides.length];
   return (
     <>
-      <div className="embla">
+      <div className="embla cursor-pointer">
         <div className="embla__viewport" ref={emblaRef}>
           <div className="embla__container">
             {slides.map((image, index) => (
