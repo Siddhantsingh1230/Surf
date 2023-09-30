eb79e05920f07d1627db783910e163c2ad193fbc
Siddhantsingh1230 <Siddhantsingh9023134084@gmail.com>
2023-09-30 22:49:55 +0530

    improved UX

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -257,7 +257,7 @@ const Product = () => {
           </Transition.Root>
 
           <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-            <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
+            <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-3">
               <h1
                 className="text-4xl font-bold tracking-tight text-gray-900"
                 ref={productTop}
