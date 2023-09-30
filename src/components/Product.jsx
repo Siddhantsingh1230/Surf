f536b5e19e382b5a1b5e9aadf2e039703e78792b
Siddhantsingh1230 <Siddhantsingh9023134084@gmail.com>
2023-09-30 09:15:59 +0530

    added Spinner

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -123,73 +123,71 @@ const Product = () => {
                     <form className="mt-4 border-t border-gray-200">
                       <h3 className="sr-only">Categories</h3>
 
-                      {categoriesStatus === "loading" ||
-                      brandsStatus === "loading" ? (
-                        <Spinner />
-                      ) : (
-                        filters.map((section) => (
-                          <Disclosure
-                            as="div"
-                            key={section.id}
-                            className="border-t border-gray-200 px-4 py-6"
-                          >
-                            {({ open }) => (
-                              <>
-                                <h3 className="-mx-2 -my-3 flow-root">
-                                  <Disclosure.Button className="flex w-full items-center justify-between bg-white px-2 py-3 text-gray-400 hover:text-gray-500">
-                                    <span className="font-medium text-gray-900">
-                                      {section.name}
-                                    </span>
-                                    <span className="ml-6 flex items-center">
-                                      {open ? (
-                                        <MinusIcon
-                                          className="h-5 w-5"
-                                          aria-hidden="true"
-                                        />
-                                      ) : (
-                                        <PlusIcon
-                                          className="h-5 w-5"
-                                          aria-hidden="true"
-                                        />
-                                      )}
-                                    </span>
-                                  </Disclosure.Button>
-                                </h3>
-                                <Disclosure.Panel className="pt-6">
-                                  <div className="space-y-6">
-                                    {section.options.map(
-                                      (option, optionIdx) => (
-                                        <div
-                                          key={option.value}
-                                          className="flex items-center"
-                                        >
-                                          <input
-                                            id={`filter-mobile-${section.id}-${optionIdx}`}
-                                            name={`${section.id}[]`}
-                                            defaultValue={option.value}
-                                            type="checkbox"
-                                            defaultChecked={option.checked}
-                                            onChange={(e) =>
-                                              handleFilter(e, section, option)
-                                            }
-                                            className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                                          />
-                                          <label
-                                            htmlFor={`filter-mobile-${section.id}-${optionIdx}`}
-                                            className="ml-3 min-w-0 flex-1 text-gray-500"
-                                          >
-                                            {option.label}
-                                          </label>
-                                        </div>
-                                      )
+                      {filters.map((section) => (
+                        <Disclosure
+                          as="div"
+                          key={section.id}
+                          className="border-t border-gray-200 px-4 py-6"
+                        >
+                          {({ open }) => (
+                            <>
+                              <h3 className="-mx-2 -my-3 flow-root">
+                                <Disclosure.Button className="flex w-full items-center justify-between bg-white px-2 py-3 text-gray-400 hover:text-gray-500">
+                                  <span className="font-medium text-gray-900">
+                                    {section.name}
+                                  </span>
+                                  <span className="ml-6 flex items-center">
+                                    {open ? (
+                                      <MinusIcon
+                                        className="h-5 w-5"
+                                        aria-hidden="true"
+                                      />
+                                    ) : (
+                                      <PlusIcon
+                                        className="h-5 w-5"
+                                        aria-hidden="true"
+                                      />
                                     )}
-                                  </div>
-                                </Disclosure.Panel>
-                              </>
-                            )}
-                          </Disclosure>
-                        ))
-                      )}
+                                  </span>
+                                </Disclosure.Button>
+                              </h3>
+                              <Disclosure.Panel className="pt-6">
+                                <div className="space-y-6">
+                                  {categoriesStatus === "loading" ||
+                                  brandsStatus === "loading" ? (
+                                    <Spinner />
+                                  ) : (
+                                    section.options.map((option, optionIdx) => (
+                                      <div
+                                        key={option.value}
+                                        className="flex items-center"
+                                      >
+                                        <input
+                                          id={`filter-mobile-${section.id}-${optionIdx}`}
+                                          name={`${section.id}[]`}
+                                          defaultValue={option.value}
+                                          type="checkbox"
+                                          defaultChecked={option.checked}
+                                          onChange={(e) =>
+                                            handleFilter(e, section, option)
+                                          }
+                                          className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                                        />
+                                        <label
+                                          htmlFor={`filter-mobile-${section.id}-${optionIdx}`}
+                                          className="ml-3 min-w-0 flex-1 text-gray-500"
+                                        >
+                                          {option.label}
+                                        </label>
+                                      </div>
+                                    ))
+                                  )}
+                                </div>
+                              </Disclosure.Panel>
+                            </>
+                          )}
+                        </Disclosure>
+                      ))}
                     </form>
                   </Dialog.Panel>
                 </Transition.Child>
@@ -276,41 +274,41 @@ const Product = () => {
                 {/* Filters */}
                 <form className="hidden lg:block">
                   <h3 className="sr-only">Categories</h3>
-                  {categoriesStatus === "loading" ||
-                  brandsStatus === "loading" ? (
-                    <Spinner />
-                  ) : (
-                    filters.map((section) => (
-                      <Disclosure
-                        as="div"
-                        key={section.id}
-                        className="border-b border-gray-200 py-6"
-                      >
-                        {({ open }) => (
-                          <>
-                            <h3 className="-my-3 flow-root">
-                              <Disclosure.Button className="flex w-full items-center justify-between bg-white rounded-md px-2 hover:bg-gray-100 py-3 text-sm text-gray-400 hover:text-gray-500">
-                                <span className="font-medium text-gray-900">
-                                  {section.name}
-                                </span>
-                                <span className="ml-6 flex items-center">
-                                  {open ? (
-                                    <MinusIcon
-                                      className="h-5 w-5"
-                                      aria-hidden="true"
-                                    />
-                                  ) : (
-                                    <PlusIcon
-                                      className="h-5 w-5"
-                                      aria-hidden="true"
-                                    />
-                                  )}
-                                </span>
-                              </Disclosure.Button>
-                            </h3>
-                            <Disclosure.Panel className="pt-6">
-                              <div className="space-y-4">
-                                {section.options.map((option, optionIdx) => (
+                  {filters.map((section) => (
+                    <Disclosure
+                      as="div"
+                      key={section.id}
+                      className="border-b border-gray-200 py-6"
+                    >
+                      {({ open }) => (
+                        <>
+                          <h3 className="-my-3 flow-root">
+                            <Disclosure.Button className="flex w-full items-center justify-between bg-white rounded-md px-2 hover:bg-gray-100 py-3 text-sm text-gray-400 hover:text-gray-500">
+                              <span className="font-medium text-gray-900">
+                                {section.name}
+                              </span>
+                              <span className="ml-6 flex items-center">
+                                {open ? (
+                                  <MinusIcon
+                                    className="h-5 w-5"
+                                    aria-hidden="true"
+                                  />
+                                ) : (
+                                  <PlusIcon
+                                    className="h-5 w-5"
+                                    aria-hidden="true"
+                                  />
+                                )}
+                              </span>
+                            </Disclosure.Button>
+                          </h3>
+                          <Disclosure.Panel className="pt-6">
+                            <div className="space-y-4">
+                              {categoriesStatus === "loading" ||
+                              brandsStatus === "loading" ? (
+                                <Spinner />
+                              ) : (
+                                section.options.map((option, optionIdx) => (
                                   <div
                                     key={option.value}
                                     className="flex items-center"
@@ -333,14 +331,14 @@ const Product = () => {
                                       {option.label}
                                     </label>
                                   </div>
-                                ))}
-                              </div>
-                            </Disclosure.Panel>
-                          </>
-                        )}
-                      </Disclosure>
-                    ))
-                  )}
+                                ))
+                              )}
+                            </div>
+                          </Disclosure.Panel>
+                        </>
+                      )}
+                    </Disclosure>
+                  ))}
                 </form>
                 {/* Product grid */}
                 <div className="lg:col-span-3">
--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -4,7 +4,7 @@ const Spinner = () => {
   return (
     <>
       <div className="w-full flex justify-center h-full items-center ">
-        <svg xmlns="http://www.w3.org/2000/svg" className="goog-te-spinner w-7 sm:w-[3%]"  height="auto" viewBox="0 0 66 66"><circle className="goog-te-spinner-path" fill="none" strokeWidth="6" strokeLinecap="round" cx="33" cy="33" r="30"></circle></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" className="goog-te-spinner w-7 sm:w-[3%]"  viewBox="0 0 66 66"><circle className="goog-te-spinner-path" fill="none" strokeWidth="6" strokeLinecap="round" cx="33" cy="33" r="30"></circle></svg>
       </div>
     </>
   );
